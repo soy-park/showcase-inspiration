4cf644aed430299453e07bb46444631f1397f855
Sara Park <sspark106@gmail.com>
2023-06-05 15:22:51 -0600

    feat: Add componentDidMount() that fetches quotes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,23 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      list: []
+      quotes: []
     }
+  }
+
+  componentDidMount = () => {
+    return fetch("https://quote-garden.onrender.com/api/v3/quotes")
+      .then(response => {
+        if (!response.ok) {
+         throw new Error(`${response.status}, ${response.statusText}`)
+        }
+        return response.json()
+    }) 
+      .then(data => {
+        console.log(data)
+        this.setState({ quotes: data })
+      })
+      .catch(err => {throw new Error(`${err}`)})
   }
 }
 
