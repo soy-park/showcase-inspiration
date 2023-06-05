f2d9f194a3bb4021a1b2032bf9eacbcf61d82d3e
Sara Park <sspark106@gmail.com>
2023-06-05 15:59:29 -0600

    feat: Add BrowserRouter in index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch, NavLink } from "react-router-dom";
+import {Route, Switch, NavLink} from "react-router-dom";
 import './App.css';
 
 class App extends Component {
