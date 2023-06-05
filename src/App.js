71fc63ef77645555ddfee74a83f743f83ade46e0
Sara Park <sspark106@gmail.com>
2023-06-05 15:18:38 -0600

    feat: Start App Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React, { Component } from "react";
 import './App.css';
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: []
+    }
+  }
+}
 
+export default App;
