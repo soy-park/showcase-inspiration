30498595f8f23a1c6408130c9f9de4dcaf49cb0a
Sara Park <sspark106@gmail.com>
2023-06-07 15:14:47 -0600

    feat: Add styling (border, color, margins) to NotFoundPage

--- src/Components/NotFoundPage/NotFoundPage.js
+++ src/Components/NotFoundPage/NotFoundPage.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import '../NotFoundPage/NotFoundPage.css';
 
 function NotFoundPage() {
 
     return (
-        <main>
-            <h3>Page was not found</h3>
-            <NavLink to="/">Click here for your Dose of Quotes!</NavLink>
+        <main className="page-not-found">
+            <h2 className="page-not-found-header">Page was not found</h2>
+            <p className="not-found-paragraph">Please click on "All Quotes" above to be directed to your dose!</p>
         </main>
     )
 }
