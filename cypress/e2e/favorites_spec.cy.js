766488c44e7fffaaa3fc20fc9eac50895bf4eef9
Sara Park <sspark106@gmail.com>
2023-06-08 15:29:32 -0600

    feat: Create favorites spec to test favorites page

---
+++ cypress/e2e/favorites_spec.cy.js
@@ -0,0 +1,8 @@
+describe('Main Page', () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://quote-garden.onrender.com/api/v3/quotes", {
+      statusCode: 200,
+      fixture: "quotes.json"})
+      .visit("http://localhost:3000/favorites")
+  })
+})
