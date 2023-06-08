17af25e786aeaa4a9b21063c19d89d370ad82d94
Sara Park <sspark106@gmail.com>
2023-06-08 13:36:42 -0600

    feat: Change fixture data so that it's an object of an object of array

--- cypress/e2e/main_spec.cy.js
+++ cypress/e2e/main_spec.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Main Page', () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://quote-garden.onrender.com/api/v3/quotes", {
+      statusCode: 200,
+      fixture: "quotes.json"})
+      .visit("http://localhost:3000/")
   })
+
+  it('should go to a base url', () => {
+    cy.url().should('include', '/')
+  })
+
+  // it('should render a heading', () => {
+  //   cy.contains("h1", "POKEMON CARDS!")
+  //   cy.contains("h3", "Browse cards and build your deck")
+  // })
 })
